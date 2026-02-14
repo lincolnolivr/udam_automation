6fcf0c39c88328afbf8391f811bf6aac951fe16e
Lincoln Oliver <lincolnolive@gmail.com>
2026-02-14 17:18:09 -0300

    6

--- main.py
+++ main.py
@@ -328,7 +328,7 @@ if __name__ == "__main__":
         digitar(data_formatada)
 
         # Enter 5 vezes
-        for _ in range(5):
+        for _ in range(6):
             pressionar('enter')
 
         # Próxima data
